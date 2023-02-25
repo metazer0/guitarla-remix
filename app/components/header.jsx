464943237d88830e14f2be81356e9e1b0fb95ead
Isaac David <isaacdvid15.96@outlook.com>
2023-02-25 12:40:22 -0600

    added header links and image and other routes.

---
+++ app/components/header.jsx
@@ -0,0 +1,38 @@
+import { Link } from '@remix-run/react'
+import Logo from '../../public/img/logo.svg'
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="contenedor barra">
+        <Link to="/">
+          <img className='logo' src={Logo} alt="Imagen Logo" />
+        </Link>
+        <nav className="navegacion">
+            <Link
+              to="/"
+            >
+              Inicio
+            </Link>
+            <Link
+              to="/nosotros"
+            >
+              Nosotros
+            </Link>
+            <Link
+              to="/tienda"
+            >
+              Tienda
+            </Link>
+            <Link
+              to="/blog"
+            >
+              Blog
+            </Link>
+          </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ app/routes/index.jsx
@@ -0,0 +1,7 @@
+function Index() {
+  return (
+    <div>desde index.jsx</div>
+  )
+}
+
+export default Index
